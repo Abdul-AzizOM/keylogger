e4258bda4cb9e5a3dbbfcd1b5d031e802d1ea21b
Abdul-AzizOM <abdulazizoffcl@gmail.com>
2026-03-02 21:42:41 +0400

    read the input file

--- main.cpp
+++ main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
 #include <filesystem>
+#include <fstream>
+
+std::filesystem::path path = "/dev/input";//the inputs folder for linux
+std::string keys_input_file = "event7";
 
 
-std::filesystem::path path = "/dev/input";
+bool change_dr(std::filesystem::path path);
 
-
-
-bool change_dr();
-
-int main(){
-    if(!(change_dr)){
+int main()
+{
+  if(!(change_dr(path))){
       std::cout << "Path not found";
-      return 0;
-    }
-    std::cout << "HI" << std::endl;
-    return 1;
+      return 0;  
+  }
+  
+  
+  std::ifstream file;
+  file.open(keys_input_file , std::ios::binary);
+
+  int c = file.get();
+  
+
+  while (file.good()){
+    std::cout << std::hex <<  c << std::setw(8)<<std::endl;
+    c = file.get();
+  }
+
+  std::cout << "HI" << std::endl;
+  return 1;
 }
 
 
 
-bool change_dr(){
+bool change_dr(std::filesystem::path path){
     if(std::filesystem::exists(path))
     {
     std::filesystem::current_path(path);
@@ -27,3 +41,6 @@ bool change_dr(){
     }
     return false;
 }
+
+
+
